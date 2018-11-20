454d4e090328dbb395b8d34a3edf357c0a2c6bd6
Qirui-UBC <44004091+Qirui-UBC@users.noreply.github.com>
2018-11-19 17:53:13 -0800

    Change

--- src/Newtonsmethod.jl
+++ src/Newtonsmethod.jl
@@ -4,13 +4,13 @@ greet() = print("Hello World!")
 
 using Expectations, Distributions, ForwardDiff, LinearAlgebra
 
-function Newtonsmethod(f, f', x_0, tolerance = 1E-7, maxiter = 1000)
+function newtonroot(f, f′; x₀, tolerance = 1E-7, maxiter = 1000)
     # setup the algorithm
-    x_old = x_0
+    x_old = x₀
     normdiff = Inf
     iter = 1
     while normdiff > tolerance && iter <= maxiter
-        x_new = x_old - f(x_old)/f'(x_old)
+        x_new = x_old - f(x_old)/f′(x_old)
         normdiff = norm(x_new - x_old)
         x_old = x_new
         iter = iter + 1
@@ -18,21 +18,10 @@ function Newtonsmethod(f, f', x_0, tolerance = 1E-7, maxiter = 1000)
     return (value = x_old, normdiff = normdiff, iter = iter)
 end
 
-function Newtonsmethod(f, x_0, tolerance = 1E-7, maxiter = 1000)
-    D(f) = x -> ForwardDiff.derivative(f, x)
-    f_prime = D(f)
-    x_old = x_0
-    normdiff = Inf
-    iter = 1
-    while normdiff > tolerance && iter <= maxiter
-        x_new = x_old - f(x_old)/f_prime(x_old)
-        normdiff = norm(x_new - x_old)
-        x_old = x_new
-        iter = iter + 1
-    end
-    return (value = x_old, normdiff = normdiff, iter = iter)
-end
+D(f) = x -> ForwardDiff.derivative(f, x)
 
+newtonroot(f; x₀, tolerance = 1E-7, maxiter = 1000) = newtonroot(f, D(f); x₀ = x₀, tolerance = 1E-7, maxiter = 1000)
 
+export newtonroot
 
 end # module
