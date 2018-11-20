5c07b88edb63943d98fe4b59e3c4b9e5d98674fd
Qirui-UBC <44004091+Qirui-UBC@users.noreply.github.com>
2018-11-19 17:08:28 -0800

    Update Newtonsmethod.jl

--- src/Newtonsmethod.jl
+++ src/Newtonsmethod.jl
@@ -2,14 +2,37 @@ module Newtonsmethod
 
 greet() = print("Hello World!")
 
-using Expectations, Distributions
+using Expectations, Distributions, ForwardDiff, LinearAlgebra
 
-function foo(μ = 1., σ = 2.)
-    d = Normal(μ, σ)
-    E = expectation(d)
-    return E(x -> sin(x))
+function Newtonsmethod(f, f', x_0, tolerance = 1E-7, maxiter = 1000)
+    # setup the algorithm
+    x_old = x_0
+    normdiff = Inf
+    iter = 1
+    while normdiff > tolerance && iter <= maxiter
+        x_new = x_old - f(x_old)/f'(x_old)
+        normdiff = norm(x_new - x_old)
+        x_old = x_new
+        iter = iter + 1
+    end
+    return (value = x_old, normdiff = normdiff, iter = iter)
 end
 
-export foo
+function Newtonsmethod(f, x_0, tolerance = 1E-7, maxiter = 1000)
+    D(f) = x -> ForwardDiff.derivative(f, x)
+    f_prime = D(f)
+    x_old = x_0
+    normdiff = Inf
+    iter = 1
+    while normdiff > tolerance && iter <= maxiter
+        x_new = x_old - f(x_old)/f_prime(x_old)
+        normdiff = norm(x_new - x_old)
+        x_old = x_new
+        iter = iter + 1
+    end
+    return (value = x_old, normdiff = normdiff, iter = iter)
+end
+
+
 
 end # module
