483ab3b5e3dfc899b0e955e9672f9822bfe91b3d
Qirui-UBC <44004091+Qirui-UBC@users.noreply.github.com>
2018-11-20 15:52:52 -0800

    Update runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,6 @@
 using Newtonsmethod
 using Test
+using LinearAlgebra
 
 # Testing the first function (unit test)
 f(x) = x^2-16.0
@@ -42,12 +43,12 @@ b=newtonroot(f;x₀=10.0,maxiter=5)
 end;
 
 # Testing tolerance level
-f(x) = exp(x) - 2.5
-x₁ = newtonroot(f;x₀=100.0)[1]
-x₂ = newtonroot(f;x₀=100.0,tolerance=1E-3)[1]
-x₃ = newtonroot(f;x₀=100.0,tolerance=1)[1]
-@test "tolerance" begin
-    @test norm(f(x₁)) < norm(f(x₂))
-    @test norm(f(x₁)) < norm(f(x₃))
-    @test norm(f(x₂)) < norm(f(x₃))
+g(x) = exp(x) - 2.5
+x₁ = newtonroot(g;x₀=10.0)[1]
+x₂ = newtonroot(g;x₀=10.0,tolerance=1E-3)[1]
+x₃ = newtonroot(g;x₀=10.0,tolerance=1)[1]
+@testset "tolerance" begin
+    @test norm(g(x₁)) < norm(g(x₂))
+    @test norm(g(x₁)) < norm(g(x₃))
+    @test norm(g(x₂)) < norm(g(x₃))
 end;
